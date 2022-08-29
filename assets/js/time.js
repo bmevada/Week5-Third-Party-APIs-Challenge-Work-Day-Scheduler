908d7366c0dae2741ba30eda1963e78522a788f1
Bhavika Mevada <bmevada>
2022-08-29 21:31:21 +1000

    add retrieve data from storage

--- assets/js/time.js
+++ assets/js/time.js
@@ -42,5 +42,17 @@ $(document).ready(function () {
     
 
   //Retrieve events from local storage to add to planner
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
 
-}
+    timeTracker();
+})
