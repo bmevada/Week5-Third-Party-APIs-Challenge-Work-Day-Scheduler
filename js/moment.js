41cb5fa71ce7b621feaad5980f08ec8d4d3598b1
Bhavika Mevada <bmevada>
2022-08-29 18:48:11 +1000

    draft events for moment.js

---
+++ js/moment.js
@@ -0,0 +1,19 @@
+// Add moment.js file for current date and time
+$(document).ready(function () {
+  let NowMoment = moment().format("MMMM Do YYYY");
+  let displayDate = document.getElementById("currentDay");
+  displayDate.innerHTML = NowMoment;
+  let currentHour = moment().format("HH");
+
+    // Onclick clear local storage and clear contents
+
+
+    //Take time from the planner and compare with current time
+
+
+    //Add time and events to local storage
+
+
+    //Retrieve events from local storage to add to planner
+
+}
